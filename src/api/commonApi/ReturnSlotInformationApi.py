d44aa2b058e067d4d4d1ea8ddc7b05036d54b213
aa3pankaj <aa3pankaj@gmail.com>
2020-04-10 03:20:58 +0530

    gn

--- src/api/commonApi/ReturnSlotInformationApi.py
+++ src/api/commonApi/ReturnSlotInformationApi.py
@@ -12,41 +12,57 @@ from sqlalchemy import or_
 import random,string
 import calendar;
 import time;
-from datetime import datetime,timedelta
+from datetime import datetime,date,timedelta
 from sqlalchemy import and_
+from models.model import Merchant
 
 class ReturnSlotInformationApi(Resource):
 
     def post(self):
-        data = request.data
-        slot_id = data["slot_id"]
-        police_user_id = data["police_user_id"]
+        request_data = request.data
+        slot_id = request_data["slot_id"]
+        #police_user_id = data["police_user_id"]
        
         try:
             slot = Slot.query.get(slot_id)
+            slotInfo={}
             if(slot):   
-                slotInfo["booking_date"]=str(slot.booking_date) 
-                slotInfo["start_time"]=slot.startTime
+               
+                slotInfo["booking_date"]=str(slot.booking_date.date()) 
+                slotInfo["start_time"]=slot.startime
                 slotInfo["end_time"]=slot.endTime
+               
                 slot_user_id=slot.user_id
                 merchant_id=slot.merchant_id 
-                now = datetime.now()
-                if(slot.booking_date<now):
-                    slotInfo["status"]="invalid"
+                hourNow=datetime.now().hour
+                now = datetime.now().date()
+                #timestamp = datetime.now()
+               
+                merchantInfo={}
+                if(slot.booking_date.date()<now):
+                    return self.response("200","true","","invalid")
                 else:
-                    slotInfo["status"]="valid"
+                   
+                    if slot.booking_date==now:
+                        if slot.startime < hourNow:
+                            return self.response("200","true","","invalid")
+            
                     merchant=Merchant.query.get(merchant_id)
                     merchantInfo["merchant_id"]=merchant_id
                     merchantInfo["shop_name"]=merchant.shopName
                     merchantInfo["shop_category"]=merchant.shopCategory
                     merchantInfo["max_people_per_slot"]=merchant.maxPeoplePerSlot
-                    merchantInfo["lat"]=merchant.lat
-                    merchantInfo["lng"]=merchant.lng
+                    merchantInfo["lat"]=str(merchant.lat)
+                    merchantInfo["lng"]=str(merchant.lng)
+                  
                 data={}
+                
                 data["merchantInfo"]=merchantInfo
                 data["slot_info"]=slotInfo
-                message="OK"
+                message="valid"
                 return self.response("200","false",data,message)
+            else:
+                return self.response("200","true","","invalid")
         except Exception as err:
             message = err
             return self.response("503","true", {}, message)
