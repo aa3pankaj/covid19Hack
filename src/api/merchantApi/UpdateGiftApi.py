f32044004ab1fc2064e2bf1eba603bef58bc7131
Pankaj Singh <aa3pankaj@gmail.com>
2020-04-11 03:56:28 +0530

    added crud for gift (#2)

---
+++ src/api/merchantApi/UpdateGiftApi.py
@@ -0,0 +1,44 @@
+import subprocess, os, time
+import re
+from flask_restful import Resource, Api, reqparse
+from flask import request
+import logging, threading
+import shutil
+from models.model import Slot
+from utils.database import db
+import json
+import copy
+from sqlalchemy import or_
+import random,string
+import calendar;
+import time;
+from datetime import datetime,timedelta
+from sqlalchemy import and_
+from models.model import Merchant_Gift
+
+class UpdateGiftApi(Resource):
+
+    def post(self):
+        request_data = request.data
+        merchant_id = request_data["merchant_id"]
+        amount = request_data["amount"]
+        gift_name = request_data["gift_name"]
+        try: 
+            gift=Merchant_Gift.query.filter_by(gift_name=gift_name).first()
+            gift.amount = amount
+            db.session.add(gift)
+            db.session.commit()
+            message = "Success"
+            return self.response("200","false",{"gift_id":gift.id,"gift_name":gift.gift_name,"amount":gift.amount},message)
+        except Exception as err:
+            message = str(err)
+            return self.response("503", "true",{}, message)
+
+
+    def response(self, responseCode,hasError,data,message):
+        response = {}
+        response['responseCode'] = responseCode
+        response['message'] = message
+        response['hasError'] = hasError
+        response['data'] = data
+        return response
