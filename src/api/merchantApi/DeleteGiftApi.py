c940b03c70dddd60c712b6064aa603c7f2ce7742
aa3pankaj <aa3pankaj@gmail.com>
2020-04-12 19:03:39 +0530

    gift update

--- src/api/merchantApi/DeleteGiftApi.py
+++ src/api/merchantApi/DeleteGiftApi.py
@@ -23,8 +23,8 @@ class DeleteGiftApi(Resource):
         try:
             data = request.data
             merchant_id = data["merchant_id"]
-            gift_name = data["gift_name"]     
-            gift=Merchant_Gift.query.filter_by(gift_name=gift_name, merchant_id = merchant_id, status = "active").first()
+            gift_id = data["gift_id"]    
+            gift=Merchant_Gift.query.filter_by(id=gift_id, merchant_id = merchant_id, status = "active").first()
             if gift:
                 gift.status = "inactive"
                 message = "success"
